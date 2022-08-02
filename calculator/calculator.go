f0cd7c5c0a5f851c4c543998289e6b829cdc7058
İdil Dikmen <ilaydaidil@sabanciuniv.edu>
2022-08-02 14:27:51 +0200

    please

---
+++ calculator/calculator.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+
+func main()
+{
+
+	num1 := 0
+	num2 := 0
+
+	fmt.Println("Enter The First Number: ")
+	fmt.Scanln(&num1)
+	fmt.Println("Enter The Second Number: ")
+	fmt.Scanln(&num2)
+
+	fmt.Println(num1+num2)
+
+}
